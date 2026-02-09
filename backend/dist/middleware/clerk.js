a8e244a683281cab5465d130ff7e2fdedd6897f6
Amritesh Singh <amritesh6767@gmail.com>
2026-02-09 17:21:46 +0530

    deleted unnecessary files

--- backend/dist/middleware/clerk.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.clerkMiddleware = void 0;
-const clerk_sdk_node_1 = require("@clerk/clerk-sdk-node");
-// Middleware to authenticate requests using Clerk
-exports.clerkMiddleware = (0, clerk_sdk_node_1.ClerkExpressWithAuth)({
-// Optional: Add configuration here if needed, 
-// but it should auto-pick from CLERK_SECRET_KEY in .env
-});
