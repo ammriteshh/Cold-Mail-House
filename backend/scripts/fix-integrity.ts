7efa36998fbb461555e9dd0322e6b56832190de0
Amritesh Singh <amritesh6767@gmail.com>
2026-02-18 23:32:58 +0530

    commit

--- backend/scripts/fix-integrity.ts
+++ backend/scripts/fix-integrity.ts
@@ -2,11 +2,7 @@ import { PrismaClient } from '@prisma/client';
 import 'dotenv/config';
 
 const prisma = new PrismaClient({
-    datasources: {
-        db: {
-            url: process.env.DATABASE_URL,
-        },
-    },
+    datasourceUrl: process.env.DATABASE_URL,
 });
 
 async function main() {
