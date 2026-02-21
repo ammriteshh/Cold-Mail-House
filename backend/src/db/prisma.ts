5e4a71e8ba28b0e260d06cfd2931ece60ef25374
Amritesh Singh <amritesh6767@gmail.com>
2026-02-21 18:13:27 +0530

    commit

--- backend/src/db/prisma.ts
+++ backend/src/db/prisma.ts
@@ -1,20 +1,10 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
 
 // Prevent multiple instances of Prisma Client in development
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const connectionString = process.env.DATABASE_URL;
-
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter,
-    log: ['query'],
-  });
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
